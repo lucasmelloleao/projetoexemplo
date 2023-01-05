de6d2c6abb7408ecb43ab250a94c825f275fc924
Lucas Leão <lucasmelloleao@gmail.com>
2023-01-05 14:45:49 -0300

    m

--- src/routes.ts
+++ src/routes.ts
@@ -32,6 +32,7 @@ const upload = multer(uploadConfig.upload("./tmp"));
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 
+console.log ("teste lucas")
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
